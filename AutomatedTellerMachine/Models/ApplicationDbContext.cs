127e933049fd7f9ec848a7dd7727dc76941b76de
teodor <savateodor@yahoo.co.uk>
2016-08-30 20:18:22 +0200

    super nice

--- AutomatedTellerMachine/Models/ApplicationDbContext.cs
+++ AutomatedTellerMachine/Models/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using AutomatedTellerMachine.Migrations;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace AutomatedTellerMachine.Models
@@ -26,10 +27,16 @@ namespace AutomatedTellerMachine.Models
             {
                 return new Models.ApplicationDbContext();
             }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext,Configuration>());
+            base.OnModelCreating(modelBuilder); 
+        }
             public IDbSet<CheckingAccount> Checking { get; set; }
 
             public IDbSet<Transaction> Transactions { get; set; }
-        
+            
     }
 
     public class  FakeApplicationDbContext : IApplicationDbContext
